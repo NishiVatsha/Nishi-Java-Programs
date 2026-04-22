123555eafaa1c825433f0ef62830dd0dada1ad7b
Nishi Vatsha <nishivatsha@gmail.com>
2026-04-22 08:27:01 +0530

    Add files via upload

---
+++ ThreadExample.java
@@ -0,0 +1,21 @@
+// 9. Multithreading using Thread class
+
+class MyThread extends Thread {
+    public void run() {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+    }
+}
+
+public class ThreadExample {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        MyThread t2 = new MyThread();
+        MyThread t3 = new MyThread();
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
