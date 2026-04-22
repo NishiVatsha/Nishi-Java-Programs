eec5c4a2ab1571cefd4f4083bbfe54168eda00e2
Nishi Vatsha <nishivatsha@gmail.com>
2026-04-22 10:33:01 +0530

    Add files via upload

---
+++ Inheritance.java
@@ -0,0 +1,24 @@
+// 22.  Inheritance Programs, using interface and abstract classes.
+
+
+abstract class Animal {
+    abstract void sound();  
+
+    void eat() {            
+        System.out.println("Animal eats food");
+    }
+}
+
+class Dog extends Animal {
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class Inheritance {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.sound();
+        d.eat();
+    }
+}
